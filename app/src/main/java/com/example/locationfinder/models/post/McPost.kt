9432283bb8d480ded507426c5eb32ebdde4ee0cb
rlaal <rlaalgus132@naver.com>
2021-05-11 01:41:42 +0900

    upload image test

--- app/src/main/java/com/example/locationfinder/models/post/McPost.kt
+++ app/src/main/java/com/example/locationfinder/models/post/McPost.kt
@@ -1,14 +1,15 @@
 package com.example.locationfinder.models.post
 
+import android.net.Uri
+
 data class McPost(
-    val id: Int,
-    val title: String,
+    val id: Long,
     val description: String,  // 사진,동영상의 설명
-    val contents : String, //사진과 같이 올린 내용
+    val contents: String, //사진과 같이 올린 내용
     val like: Int,
     val reply: List<Review>,
     val date: String,
-    val url : String,
+    val url: MutableList<Uri>,
     val dataType:Int,
-    val tag : List<String>
+    val tag: List<String>
 )
--- app/src/main/java/com/example/locationfinder/ui/schedule/MakeNewPostFragment.kt
+++ app/src/main/java/com/example/locationfinder/ui/schedule/MakeNewPostFragment.kt
@@ -1,17 +1,15 @@
 package com.example.locationfinder.ui.schedule
 
-import android.app.ActionBar
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.example.locationfinder.BR
@@ -19,7 +17,13 @@ import com.example.locationfinder.LocationFinder
 import com.example.locationfinder.R
 import com.example.locationfinder.databinding.FragmentMakeNewPostBinding
 import com.example.locationfinder.databinding.UploadImageItemBinding
+import com.example.locationfinder.models.post.McPost
 import com.example.locationfinder.ui.base.BaseFragment
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MakeNewPostFragment : BaseFragment<FragmentMakeNewPostBinding, MakeNewPostViewModel>(),
     MakeNewPostNavigator {
@@ -31,11 +35,42 @@ class MakeNewPostFragment : BaseFragment<FragmentMakeNewPostBinding, MakeNewPost
 
     private val uploadImageUrlList = mutableListOf<Uri>()
 
+    var storage: FirebaseStorage = FirebaseStorage.getInstance()
+    var imgDownloadUrlList = mutableListOf<Uri>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding().btnUploadImage.setOnClickListener {
             checkPermissionGranted()
+        }
+
+        binding().tvUploadPost.setOnClickListener {
+            if (uploadImageUrlList.size<1) Toast.makeText(
+                context,
+                "사진을 추가해 주세요",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            //유저 정보를 가져와야함
+            
+
+            val post = McPost(
+                System.currentTimeMillis(),
+                description = binding().etContents.text.toString(),
+                contents = binding().etContents.text.toString(), 0,
+                listOf(), Calendar.DATE.toString(),
+                url = uploadImageUrlList, 0,
+                listOf()
+            )
+
+            //사진 업로드
+            //게시글 업로드
+            //게시글 상세화면으로 이동
+
+            binding().tvUploadPost.setOnClickListener {
+                uploadImageFirestore()
+            }
         }
     }
 
@@ -99,7 +134,11 @@ class MakeNewPostFragment : BaseFragment<FragmentMakeNewPostBinding, MakeNewPost
                             binding().llUploadList,
                             false
                         )
-                    val item2 = UploadImageItemBinding.inflate(LayoutInflater.from(this.context),binding().llUploadList,false)
+                    val item2 = UploadImageItemBinding.inflate(
+                        LayoutInflater.from(this.context),
+                        binding().llUploadList,
+                        false
+                    )
                     item2.ivUpload.setImageURI(it)
                     //추가한 아이템 리스트에 저장하고, 레이아웃에 뷰 추가0
                     // x 버튼 클릭하면 아이템과 저장목록 지우기.
@@ -108,6 +147,22 @@ class MakeNewPostFragment : BaseFragment<FragmentMakeNewPostBinding, MakeNewPost
                     }
                     binding().llUploadList.addView(item2.uploadItem)
                 }
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun uploadImageFirestore(){
+        var storageRef = storage.reference.child("images")
+
+        for(img in uploadImageUrlList){
+            var imgFileName = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())+img+".png"
+            storageRef.child(imgFileName)
+            storageRef.putFile(img).addOnSuccessListener {
+                storageRef.downloadUrl.result?.let { it1 -> imgDownloadUrlList.add(it1) }
+                Log.d("upload file Success: ",img.toString() )
+            }.addOnFailureListener {
+                Log.d("upload file Fail!!!: ",img.toString() )
             }
         }
     }
